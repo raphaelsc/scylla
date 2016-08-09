68857d4d3e918f625a58ce170fffc228dbca4072
Raphael S. Carvalho <raphaelsc@scylladb.com>
2016-08-09 16:08:10 -0300

    sstables: store schema in sstable object
    That will be needed for optimization that will store decorated keys
    in the sstable object, and also for a subsequent work that will
    detect wrong metadata (min/max column names) by looking at columns
    in the schema. As schema is stored in sstable, there's no longer
    a need to store ks and cf names in it.
    
    Signed-off-by: Raphael S. Carvalho <raphaelsc@scylladb.com>

--- sstables/sstables.hh
+++ sstables/sstables.hh
@@ -131,9 +131,8 @@ public:
     enum class version_types { ka, la };
     enum class format_types { big };
 public:
-    sstable(sstring ks, sstring cf, sstring dir, int64_t generation, version_types v, format_types f, gc_clock::time_point now = gc_clock::now())
-        : _ks(std::move(ks))
-        , _cf(std::move(cf))
+    sstable(schema_ptr schema, sstring dir, int64_t generation, version_types v, format_types f, gc_clock::time_point now = gc_clock::now())
+        : _schema(std::move(schema))
         , _dir(std::move(dir))
         , _generation(generation)
         , _version(v)
@@ -377,10 +376,9 @@ public:
     std::vector<sstring> component_filenames() const;
 
 private:
-    sstable(size_t wbuffer_size, sstring ks, sstring cf, sstring dir, int64_t generation, version_types v, format_types f, gc_clock::time_point now = gc_clock::now())
+    sstable(size_t wbuffer_size, schema_ptr schema, sstring dir, int64_t generation, version_types v, format_types f, gc_clock::time_point now = gc_clock::now())
         : sstable_buffer_size(wbuffer_size)
-        , _ks(std::move(ks))
-        , _cf(std::move(cf))
+        , _schema(std::move(schema))
         , _dir(std::move(dir))
         , _generation(generation)
         , _version(v)
@@ -433,8 +431,7 @@ private:
             const composite& clustering_key,
             const std::vector<bytes_view>& column_names);
 
-    sstring _ks;
-    sstring _cf;
+    schema_ptr _schema;
     sstring _dir;
     unsigned long _generation = 0;
     version_types _version;
@@ -634,7 +631,7 @@ public:
     // Used to mark a sstable for deletion that is not relevant to the current shard.
     // It doesn't mean that the sstable will be deleted, but that the sstable is not
     // relevant to the current shard, thus can be deleted by the deletion manager.
-    static void mark_sstable_for_deletion(sstring ks, sstring cf, sstring dir, int64_t generation, version_types v, format_types f);
+    static void mark_sstable_for_deletion(const schema_ptr& schema, sstring dir, int64_t generation, version_types v, format_types f);
 
     // Allow the test cases from sstable_test.cc to test private methods. We use
     // a placeholder to avoid cluttering this class too much. The sstable_test class
