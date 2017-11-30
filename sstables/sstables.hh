093a8a4f1fea11c3030f014b4f02fd6009f08ebb
Raphael S. Carvalho <raphaelsc@scylladb.com>
2017-11-30 03:29:10 -0200

    sstables: add has_correct_max_deletion_time to sstable
    Commit cc6c38324 fixes the stat. It was only updated for range
    tombstone prior to fix, so a sstable that had a regular cell with
    no expiration time could be considered fully expired which can
    lead to bad decisions in compaction for time series workloads.
    
    Signed-off-by: Raphael S. Carvalho <raphaelsc@scylladb.com>

--- sstables/sstables.hh
+++ sstables/sstables.hh
@@ -701,6 +701,11 @@ public:
         return _schema->is_compound() || !has_scylla_component() || _components->scylla_metadata->has_feature(sstable_feature::NonCompoundRangeTombstones);
     }
 
+    // Commit cc6c3832496159b1 fixes the max deletion time stat.
+    bool has_correct_max_deletion_time() const {
+        return has_scylla_component();
+    }
+
     bool filter_has_key(const key& key) {
         return _components->filter->is_present(bytes_view(key));
     }
