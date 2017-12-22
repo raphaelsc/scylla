234dc6c9d1b411ebf90932e4388aee92f85cf031
Raphael S. Carvalho <raphaelsc@scylladb.com>
2017-12-22 21:48:42 -0200

    dht: make it easier to create ring_position_view from token
    Signed-off-by: Raphael S. Carvalho <raphaelsc@scylladb.com>

--- dht/i_partitioner.hh
+++ dht/i_partitioner.hh
@@ -561,7 +561,7 @@ public:
         , _weight(bool(after_key))
     { }
 
-    ring_position_view(const dht::token& token, partition_key* key, int8_t weight)
+    ring_position_view(const dht::token& token, partition_key* key = nullptr, int8_t weight = -1)
         : _token(&token)
         , _key(key)
         , _weight(weight)
