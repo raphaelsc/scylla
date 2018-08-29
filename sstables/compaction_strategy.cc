219a94e7ccf8a132fc5403298c7b26e73c8d736a
Raphael S. Carvalho <raphaelsc@scylladb.com>
2018-08-29 18:51:37 -0300

    compaction: introduce notion of compaction-strategy-aware major compaction
    That's only the very first step which introduces the machinery for making
    major compaction aware of all strategies. By the time being, default
    implementation is used for them all which only suits size tiered.
    
    Refs #1431.
    
    Signed-off-by: Raphael S. Carvalho <raphaelsc@scylladb.com>

--- sstables/compaction_strategy.cc
+++ sstables/compaction_strategy.cc
@@ -662,6 +662,10 @@ compaction_descriptor compaction_strategy::get_sstables_for_compaction(column_fa
     return _compaction_strategy_impl->get_sstables_for_compaction(cfs, std::move(candidates));
 }
 
+compaction_descriptor compaction_strategy::get_sstables_for_major_compaction(column_family& cf, std::vector<sstables::shared_sstable> candidates) {
+    return _compaction_strategy_impl->get_sstables_for_major_compaction(cf, std::move(candidates));
+}
+
 std::vector<resharding_descriptor> compaction_strategy::get_resharding_jobs(column_family& cf, std::vector<sstables::shared_sstable> candidates) {
     return _compaction_strategy_impl->get_resharding_jobs(cf, std::move(candidates));
 }
