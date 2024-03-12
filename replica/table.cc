11dc188ba6806e3f9cba5a1696053a6200cc4226
Tomasz Grabiec <tgrabiec@scylladb.com>
2024-03-12 22:29:44 +0100

    tablets: Get rid of tablet_map::get_shard()
    Its semantics do not fit well with intra-node migration which allow
    two owning shards. Replace uses with the new has_replica() API.

--- replica/table.cc
+++ replica/table.cc
@@ -654,12 +654,12 @@ public:
         storage_group_map ret;
 
         auto& tmap = tablet_map();
+        auto local_replica = locator::tablet_replica{_my_host_id, this_shard_id()};
 
         for (auto tid : tmap.tablet_ids()) {
             auto range = tmap.get_token_range(tid);
 
-            auto shard = tmap.get_shard(tid, _my_host_id);
-            if (shard && *shard == this_shard_id()) {
+            if (tmap.has_replica(tid, local_replica)) {
                 tlogger.debug("Tablet with id {} and range {} present for {}.{}", tid, range, schema()->ks_name(), schema()->cf_name());
             }
             // FIXME: don't allocate compaction groups for tablets that aren't present in this shard.
@@ -2135,14 +2135,10 @@ int64_t table::calculate_tablet_count() const {
     const auto this_host_id = token_metadata.get_topology().my_host_id();
 
     int64_t new_tablet_count{0};
+    auto local_replica = locator::tablet_replica{this_host_id, this_shard_id()};
 
     for (auto tablet_id : tablet_map.tablet_ids()) {
-        const std::optional<shard_id> shard_id = tablet_map.get_shard(tablet_id, this_host_id);
-        if (!shard_id.has_value()) {
-            continue;
-        }
-
-        if (*shard_id == this_shard_id()) {
+        if (tablet_map.has_replica(tablet_id, local_replica)) {
             ++new_tablet_count;
         }
     }
